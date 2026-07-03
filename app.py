0de45603b3813a1c6939500ca85010f8e7541fe3
tiwari <138980045+tiwari9859@users.noreply.github.com>
2026-07-03 10:35:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import yfinance as yf
-import pandas_ta as ta
-
 st.title("📈 M.A.T.A Pro - AI Trading Tool")
-
 ticker = st.sidebar.text_input("Enter Asset", "BTC-USD")
 data = yf.download(ticker, period="1mo", interval="1d")
-
 st.subheader("Price Chart")
 st.line_chart(data['Close'])
-
 if st.button("Analyze Now"):
     st.write("Processing... AI Model Active!")
